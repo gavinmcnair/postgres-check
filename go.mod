a05aba0c3a8ff4b1f2c429799b574def4bcdec94
Gavin McNair <gavin.mcnair@kaluza.com>
2022-03-31 17:36:52 +0100

    Fix postgres check and remove password output

--- go.mod
+++ go.mod
@@ -1,9 +1,10 @@
-module github.com/gavinmcnair/prometheuscheck
+module github.com/gavinmcnair/postgrescheck
 
 go 1.17
 
 require (
 	github.com/caarlos0/env/v6 v6.9.1
+	github.com/lib/pq v1.10.4
 	github.com/prometheus/client_golang v1.12.1
 	github.com/rs/zerolog v1.26.1
 )
--- main.go
+++ main.go
@@ -1,20 +1,30 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"os"
 	"strconv"
 	"time"
 
 	"github.com/caarlos0/env/v6"
+	_ "github.com/lib/pq"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 )
 
 type config struct {
+	Database       string        `env:"DATABASE"`
+	Host           string        `env:"DB_HOST,required"`
+	Port           int           `env:"DB_PORT" envDefault:"5432"`
 	ListenPort     int           `env:"LISTEN_PORT" envDefault:"8080"`
-	RepeatInterval time.Duration `env:"REPEAT_INTERVAL" envDefault:"15s"`
+	User           string        `env:"DB_USER,required"`
+	Pass           string        `env:"DB_PASS,required"`
+	RepeatInterval time.Duration `env:"REPEAT_INTERVAL" envDefault:"0s"`
+	Ssl            string        `env:"SSLMODE" envDefault:"verify-ca"`
 }
 
 func main() {
@@ -32,24 +42,98 @@ func run() error {
 		return err
 	}
 
-	createPrometheusEndpoint(cfg).ListenAndServe()
+	user, err := returnFileContentsOrPassword(cfg.User)
+	if err != nil {
+		return err
+	}
+	pass, err := returnFileContentsOrPassword(cfg.Pass)
+	if err != nil {
+		return err
+	}
+	host, err := returnFileContentsOrPassword(cfg.Host)
+	if err != nil {
+		return err
+	}
+
+	log.Info().
+		Str("Database Host", host).
+		Int("Database Port", cfg.Port).
+		Int("Listen Port", cfg.ListenPort).
+		Str("Database User", user).
+		Str("Database Name", cfg.Database).
+		Str("Encryption", cfg.Ssl).
+		Msg("Starting Postgres Check")
+
+	go createPrometheusEndpoint(cfg).ListenAndServe()
+
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s", host, cfg.Port, user, pass)
+
+	if cfg.Database != "" {
+		psqlInfo = psqlInfo + " dbname=" + cfg.Database
+	}
+
+	if cfg.Ssl != "" {
+		psqlInfo = psqlInfo + " sslmode=" + cfg.Ssl
+	}
+
+	for {
+
+		err := connectToDatabase(psqlInfo)
+		if err != nil {
+			return err
+		}
+		if cfg.RepeatInterval == time.Duration(0) {
+			break
+		}
+
+		time.Sleep(cfg.RepeatInterval)
+	}
+
 	return nil
 }
 
-func createPrometheusEndpoint(cfg config) *http.Server {
+/* Need to check connection behaviour. We might be making lots of connections here.
+   Probably need to seperate connection from ping and pass the db object */
+func connectToDatabase(psqlInfo string) error {
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		return err
+	}
+	defer db.Close()
 
-
+	err = db.Ping()
+	if err != nil {
+		return err
+	}
 	log.Info().
- 		Int("Metrics Port", cfg.ListenPort).
-		Str("Path", "/metrics").
-		Msg("Prometheus endpoint service started.")
+		Msg("Database ping success")
+	return nil
+}
 
+func returnFileContentsOrPassword(potentialPassword string) (string, error) {
+	if _, err := os.Stat(potentialPassword); err == nil {
+		passwordFromFile, err := readFileAndReturnContents(potentialPassword)
+		if err != nil {
+			return "", err
+		}
+		return passwordFromFile, nil
+	}
+
+	return potentialPassword, nil
+}
+
+func readFileAndReturnContents(filename string) (string, error) {
+	filebytes, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return "", err
+	}
+	return string(filebytes), nil
+}
+
+func createPrometheusEndpoint(cfg config) *http.Server {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/metrics", func(w http.ResponseWriter, r *http.Request) {
-		log.Info().
-			Str("RequestFrom", r.RemoteAddr).
-			Msg("Metrics Collection has occured")
 		promhttp.Handler().ServeHTTP(w, r)
 	})
 	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
